d968f5b8f4e49a1844838207987e760aec6f6e4b
Brendan Moran <b.daniel.moran@gmail.com>
2023-07-05 15:09:25 -0400

    add $appVer variable for easier maint

--- evaluation.ps1
+++ evaluation.ps1
@@ -7,11 +7,12 @@ try {
   Exit 0
 }
 
+$appVer = "v2.1"
 $oneDrive = "C:\Users\$user\OneDrive - Senneca Holdings\Desktop"
-$deskPath = "C:\Users\$user\Desktop\JobAssignment_v2.0.exe"
-$altDeskPath = "C:\Users\$user.HMFEXPRESS\Desktop\JobAssignment_v2.0.exe"
-$oneDrivePath = "C:\Users\$user\OneDrive - Senneca Holdings\Desktop\JobAssignment_v2.0.exe"
-$altOneDrivePath = "C:\Users\$user.HMFEXPRESS\OneDrive - Senneca Holdings\Desktop\JobAssignment_v2.0.exe"
+$deskPath = "C:\Users\$user\Desktop\JobAssignment_$appVer.exe"
+$altDeskPath = "C:\Users\$user.HMFEXPRESS\Desktop\JobAssignment_$appVer.exe"
+$oneDrivePath = "C:\Users\$user\OneDrive - Senneca Holdings\Desktop\JobAssignment_$appVer.exe"
+$altOneDrivePath = "C:\Users\$user.HMFEXPRESS\OneDrive - Senneca Holdings\Desktop\JobAssignment_$appVer.exe"
 
 if (Test-Path $oneDrive) {
   if (((Test-Path $deskPath) -or (Test-Path $altDeskPath)) -and ((Test-Path $oneDrivePath) -or (Test-Path $altOneDrivePath))) {
